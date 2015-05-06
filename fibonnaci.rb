2bb222d258a538f8c34e68bd0b441193db678ad6
lukasorum <lghitu@yahoo.com>
2015-05-06 12:57:04 +0300

    delete all files

--- fibonnaci.rb
+++
@@ -1,8 +0,0 @@
-def fibonacci(max)
-  i1,i2 = 1,1
-  while i1<=max
-    yield i1
-    i1 , i2 = i2, i1+i2
-  end
-end
-fibonacci(10) {|f| print f, "\n "}
--- rbmain.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'CsvReader'
-
-reader = CsvReader.new
-reader.read_csv_book('book.csv')
-puts "#{reader.total_cost}"
-
